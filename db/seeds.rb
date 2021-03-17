efe444ff80b7f9c61cd833a6f8300f46856cad94
Joe <pickertjoe@gmail.com>
2021-03-17 14:04:28 -0500

    Seeds are up to snuff😎

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,32 +15,31 @@ end
 [
   {
     name: "Credit Score Scatter Plot",
-    statement: "SELECT credit_score, COUNT(*) FROM users GROUP BY 1"
+    statement: "SELECT credit_score, COUNT(*) FROM users GROUP BY 1",
+    status: "active"
   },
   {
     name: "Favorite Color Pie Chart",
-    statement: "SELECT favorite_color, COUNT(*) AS pie FROM users GROUP BY 1"
+    statement: "SELECT favorite_color, COUNT(*) AS pie FROM users GROUP BY 1",
+    status: "active"
   },
   {
     name: "Subscriber Status Bar Chart",
-    statement: "SELECT subscriber, COUNT(*) FROM users GROUP BY 1"
+    statement: "SELECT subscriber, COUNT(*) FROM users GROUP BY 1",
+    status: "active"
   },
 ].each_with_index do |query_params, i|
   q = Blazer::Query.find_or_initialize_by(name: query_params[:name])
-  q.creator = User.first
   q.assign_attributes(query_params)
   q.data_source = "main"
   q.save(validate: false)
 end
 
-# dashboard = Blazer::Dashboard.first_or_initialize
-# dashboard.creator = User.first
-# dashboard.update!(name: "Dashboard Demo")
-# dashboard.dashboard_queries.destroy_all
-# [
-#   "Credit Score Scatter Plot",
-#   "Favorite Color Pie Chart",
-#   "Subscriber Status Bar Chart",
-# ].each_with_index do |query_name, i|
-#   dashboard.dashboard_queries.create!(query_id: Blazer::Query.find_by(name: query_name).id, position: i)
-# end
+dashboard = Blazer::Dashboard.find_or_create_by(name: "Dashboard Demo")
+[
+  "Credit Score Scatter Plot",
+  "Favorite Color Pie Chart",
+  "Subscriber Status Bar Chart",
+].each_with_index do |query_name, i|
+  dashboard.dashboard_queries.create!(query_id: Blazer::Query.find_by(name: query_name).id, position: i)
+end
