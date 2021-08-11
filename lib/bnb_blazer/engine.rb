d4c9b13cb11050408721705e4a8349904cd8d574
William Carey <willtcarey@gmail.com>
2021-08-11 16:25:29 -0500

    Set up some styling

--- lib/bnb_blazer/engine.rb
+++ lib/bnb_blazer/engine.rb
@@ -2,5 +2,10 @@ require "blazer"
 module BnbBlazer
   class Engine < ::Rails::Engine
     isolate_namespace BnbBlazer
+
+    initializer "blazer" do |app|
+      app.config.assets.precompile << "bnb_blazer/application.css"
+      app.config.assets.precompile << "bnb_blazer/bnb_intel_logo.png"
+    end
   end
 end
