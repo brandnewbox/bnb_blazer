056b509d97c2a676a42d3c156f61af3db542ad37
ian <ianfletcher314@gmail.com>
2023-02-21 20:26:25 +0000

    tested view

--- lib/bnb_blazer/engine.rb
+++ lib/bnb_blazer/engine.rb
@@ -1,5 +1,8 @@
 require "blazer"
 module BnbBlazer
+  class << self
+    attr_accessor :mapbox_access_token
+  end
   class Engine < ::Rails::Engine
     isolate_namespace BnbBlazer
 
@@ -8,5 +11,8 @@ module BnbBlazer
       app.config.assets.precompile << "bnb_blazer/bnb_intel_logo.png"
       app.config.assets.precompile << "bnb_blazer/bnb_intel_logo_alt.png"
     end
+  end
+  def self.maps?
+    mapbox_access_token.present? && mapbox_access_token.start_with?("pk.")
   end
 end
