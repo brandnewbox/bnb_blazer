717edfddd6c65af99c698bea820b2228126d383d
ian <ianfletcher314@gmail.com>
2023-02-28 16:51:07 +0000

    updated docs code and removed method

--- lib/bnb_blazer/engine.rb
+++ lib/bnb_blazer/engine.rb
@@ -12,7 +12,4 @@ module BnbBlazer
       app.config.assets.precompile << "bnb_blazer/bnb_intel_logo_alt.png"
     end
   end
-  def self.maps?
-    mapbox_access_token.present? && mapbox_access_token.start_with?("pk.")
-  end
 end
