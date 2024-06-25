06978b69ef50aa4252396b8f934509bf12295542
ian <ian@brandnewbox.com>
2024-06-25 22:20:33 +0100

    added bootstrap

--- lib/bnb_blazer/engine.rb
+++ lib/bnb_blazer/engine.rb
@@ -4,6 +4,7 @@ module BnbBlazer
     isolate_namespace BnbBlazer
 
     initializer "blazer" do |app|
+      app.config.assets.precompile << "bnb_blazer/application.js"
       app.config.assets.precompile << "bnb_blazer/application.css"
       app.config.assets.precompile << "bnb_blazer/bnb_intel_logo.png"
       app.config.assets.precompile << "bnb_blazer/bnb_intel_logo_alt.png"
